637ef583920e2eb738f45a9a8ef153d0997f3fad
igor a <inazumaseleven04@gmail.com>
2020-07-08 16:18:51 -0300

    consegui fazer getList

--- src/App.js
+++ src/App.js
@@ -1,26 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import * as React from "react"
+import { Admin, Resource } from 'react-admin'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import { UserList } from './users'
+import { PostList, PostEdit, PostCreate} from './posts'
+import Dashboard from './Dashboard'
+import authProvider from './authProvider'
+import dataProvider from "./dataProvider"
 
-export default App;
+const App = () => (
+    <Admin dashboard={Dashboard} authProvider={authProvider} dataProvider={dataProvider}>
+        <Resource name="product" list={UserList} />
+        <Resource name="posts" list={PostList} edit={PostEdit} create={PostCreate} />
+    </Admin>
+)
+
+export default App
